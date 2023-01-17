ab5659f2686b0792cbe823f4b7ea82756e954753
Okonu <ianyakundi015@gmail.com>
2023-01-17 10:19:46 +0300

    customer table data

--- database/migrations/2023_01_17_065739_create_customers_table.php
+++ database/migrations/2023_01_17_065739_create_customers_table.php
@@ -15,6 +15,13 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type'); //indivdual or business
+            $table->string('email');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
             $table->timestamps();
         });
     }
